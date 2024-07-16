67bbc531567142b66e9b8de4dfbcac339a8c0092
estib <stron@me.com>
2024-07-16 12:51:35 +0200

    Frontend: Add a scroll area to the cafe list
    The cafe list now is scrollable, and the whole application is fixed

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,11 +2,11 @@ import CafeList from "@/components/cafe/CafeList";
 
 function App() {
   return (
-    <div className="w-screen h-screen flex flex-col fixed">
+    <div className="w-screen h-screen fixed flex flex-col">
       <header className="flex p-8 bg-primary-foreground text-primary">
         <h1 className="text-4xl font-bold">cafecito</h1>
       </header>
-      <main className="bg-primary-foreground flex-grow">
+      <main className="bg-primary-foreground overflow-hidden">
         <CafeList />
       </main>
     </div>
--- frontend/src/components/cafe/CafeList.tsx
+++ frontend/src/components/cafe/CafeList.tsx
@@ -1,36 +1,55 @@
+import React from "react";
 import Alert from "@/components/generic/Alert";
 import { useCafeList } from "@/components/hooks/cafeApi";
 import CafeCard, { CafeCardProps } from "./CafeCard";
 import SkeletonCafeCard from "./SkeletonCafeCard";
+import { ScrollArea } from "@/components/ui/scroll-area";
+
+interface CafeListContainerProps {
+  children?: React.ReactNode;
+}
+
+const CafeListContainer = (props: CafeListContainerProps) => {
+  return (
+    <div className="h-full w-full mx-auto">
+      <ScrollArea type="scroll" className="h-full w-full">
+        <div className="flex flex-col items-center p-8 space-y-8">
+          {props.children}
+        </div>
+      </ScrollArea>
+    </div>
+  );
+};
 
 const CafeList = () => {
   const { isPending, error, data } = useCafeList();
 
   if (isPending) {
     return (
-      <div className="flex-col items-center p-8 space-y-8">
+      <CafeListContainer>
         <SkeletonCafeCard />
         <SkeletonCafeCard />
-      </div>
+      </CafeListContainer>
     );
   }
 
   if (error) {
     return (
-      <div className="flex-col items-center p-8 space-y-8">
+      <CafeListContainer>
         <Alert type="error" title="Error" description={error.message} />
-      </div>
+      </CafeListContainer>
     );
   }
 
   const cafes: CafeCardProps[] = data;
 
   return (
-    <div className="flex-col items-center p-8 space-y-8">
+    <CafeListContainer>
       {cafes.map((cafe, index) => (
         <CafeCard key={index} {...cafe} />
       ))}
-    </div>
+      <div className="h-56"></div>
+    </CafeListContainer>
   );
 };
 
