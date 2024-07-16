70d5fd292f02616c72c69ceb741f2d6ffe04dce6
estib <stron@me.com>
2024-07-16 15:43:25 +0200

    Frontend: Fetch the cafe info
    The dynamic cafe route fetches the cafe info

--- frontend/src/components/hooks/cafeApi.ts
+++ frontend/src/components/hooks/cafeApi.ts
@@ -24,3 +24,55 @@ export function useCafeList() {
     queryFn: fetchCafes,
   });
 }
+
+async function fetchCafe(cafeId: string) {
+  const response = await api.cafe[":id{[0-9]+}"].$get({
+    param: { id: cafeId },
+  });
+  if (!response.ok) {
+    const data = await response.json();
+    const errorMessage = unwrappAPIError(data, "Failed to fetch cafe");
+    throw new Error(errorMessage);
+  }
+  const data = await response.json();
+
+  return data.cafe;
+}
+
+/**
+ * Custom hook for fetching cafe data.
+ * @param cafeId - The ID of the cafe to fetch.
+ * @returns The result of the query.
+ */
+export function useCafe(cafeId: string) {
+  return useQuery({
+    queryKey: ["cafe", cafeId],
+    queryFn: () => fetchCafe(cafeId),
+  });
+}
+
+async function fetchCafeInfo(cafeInfoId: string) {
+  const response = await api.cafe.info[":id{[0-9]+}"].$get({
+    param: { id: cafeInfoId },
+  });
+  if (!response.ok) {
+    const data = await response.json();
+    const errorMessage = unwrappAPIError(data, "Failed to fetch cafe info");
+    throw new Error(errorMessage);
+  }
+  const data = await response.json();
+
+  return data.cafeInfo;
+}
+
+/**
+ * Custom hook for fetching cafe info data.
+ * @param cafeInfoId - The ID of the cafe info to fetch.
+ * @returns The result of the query.
+ */
+export function useCafeInfo(cafeInfoId: string) {
+  return useQuery({
+    queryKey: ["cafeInfo", cafeInfoId],
+    queryFn: () => fetchCafeInfo(cafeInfoId),
+  });
+}
